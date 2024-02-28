d1ed1af6c322b7507449b182fc6455a5f6ae4104
max <29782397+tekoh@users.noreply.github.com>
2024-02-28 19:57:16 +0000

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,3 +8,11 @@
 <Navigation user={data.user} />
 
 <slot />
+
+<footer class="mt-14 w-full bg-black p-4 text-white">
+  <h6 class="text-center font-medium">
+    heavily inspired by <a href="https://theunsentproject.com" target="_blank" class="underline"
+      >the unsent project</a
+    >
+  </h6>
+</footer>
