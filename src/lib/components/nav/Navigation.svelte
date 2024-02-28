8c51d75820ed262d9a39b3441a71c56cbdb09832
max <29782397+tekoh@users.noreply.github.com>
2024-02-28 18:21:28 +0000

    add shadcn and nav bar

---
+++ src/lib/components/nav/Navigation.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import logo from "$lib/assets/images/logo_black.png?enhanced";
+  import { Button } from "$lib/components/ui/button";
+  import type { User } from "lucia";
+
+  export let user: User | null;
+</script>
+
+<nav class="flex w-full justify-center">
+  <div class="flex h-24 w-full max-w-3xl items-center uppercase">
+    <enhanced:img src={logo} class="h-16 w-auto" />
+    <div class="flex h-full grow items-center justify-center">
+      {#if user && user.admin}
+        <Button href="/dashboard" variant="destructive">dashboard</Button>
+      {/if}
+    </div>
+
+    <Button href="/create">create</Button>
+  </div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
+  import Navigation from "$lib/components/nav/Navigation.svelte";
   import "../app.css";
+
+  export let data;
 </script>
+
+<Navigation user={data.user} />
 
 <slot />
