97b853084d63d1373e9049b7d892abcfe31e1ebe
max <29782397+tekoh@users.noreply.github.com>
2024-03-15 19:59:22 +0000

    dont add view if self created

--- src/routes/post/[id]/+page.server.ts
+++ src/routes/post/[id]/+page.server.ts
@@ -1,5 +1,5 @@
 import db from "$lib/server/database/drizzle.js";
-import { viewsTable } from "$lib/server/database/schema.js";
+import { postTable, viewsTable } from "$lib/server/database/schema.js";
 import type { APIPost } from "$lib/types/post.js";
 import { error } from "@sveltejs/kit";
 import dayjs from "dayjs";
@@ -18,18 +18,26 @@ export async function load({ params, fetch, request, getClientAddress }) {
   return {
     post: post.post,
     _view: (async () => {
-      const query = await db
-        .select({})
-        .from(viewsTable)
-        .where(
-          and(
-            eq(viewsTable.ipAddress, getClientAddress()),
-            gt(viewsTable.createdAt, dayjs().subtract(1, "day").toDate()),
-          ),
-        )
-        .limit(1);
+      const [views, ipCheck] = await Promise.all([
+        db
+          .select({})
+          .from(viewsTable)
+          .where(
+            and(
+              eq(viewsTable.ipAddress, getClientAddress()),
+              gt(viewsTable.createdAt, dayjs().subtract(1, "day").toDate()),
+            ),
+          )
+          .limit(1),
+        db
+          .select({ ip: postTable.createdByIp })
+          .from(postTable)
+          .where(eq(postTable.id, post.post.id)),
+      ]);
 
-      if (query.length === 0)
+      if (ipCheck[0].ip === getClientAddress()) return;
+
+      if (views.length === 0)
         db.insert(viewsTable).values({ ipAddress: getClientAddress(), postId: post.post.id });
     })(),
   };
