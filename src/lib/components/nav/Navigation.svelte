d4c62ccb1101e9bbed9c6ab7e219e2c051da9650
max <29782397+tekoh@users.noreply.github.com>
2024-03-15 00:53:33 +0000

    use better img optimisation library

--- src/lib/components/nav/Navigation.svelte
+++ src/lib/components/nav/Navigation.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import logo from "$lib/assets/images/logo_black.png?enhanced";
+  import logo from "$lib/assets/images/logo_black.png?as=run:0";
   import { Button } from "$lib/components/ui/button";
+  import Img from "@zerodevx/svelte-img";
   import type { User } from "lucia";
 
   export let user: User | null;
@@ -9,7 +10,7 @@
 <nav class="flex w-full justify-center px-4">
   <div class="flex h-24 w-full max-w-3xl items-center uppercase">
     <a href="/">
-      <enhanced:img src={logo} class="h-16 w-auto" alt="logo" />
+      <Img src={logo} class="h-16 w-auto" alt="logo" />
     </a>
     <div class="flex h-full grow items-center justify-center">
       {#if user && user.admin}
