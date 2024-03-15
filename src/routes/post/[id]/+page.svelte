32ba4bfa20909fa3e91649701615702f11a70b41
max <29782397+tekoh@users.noreply.github.com>
2024-03-15 19:16:32 +0000

    title/desc

--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -4,6 +4,11 @@
   export let data;
 </script>
 
+<svelte:head>
+  <title>love note to {data.post.to} / unfiltered.love</title>
+  <meta name="description" content={data.post.text} />
+</svelte:head>
+
 <div class="mt-16 flex w-full justify-center">
   <Post post={data.post} />
 </div>
